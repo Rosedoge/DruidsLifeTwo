7b19f66d7271020f2dbfd0269f7e2216f4f7af11
Rose Flynn <cinorflynn@yahoo.com>
2018-01-25 17:17:44 -0500

    Added hub world and a single level
    Hub World
    Single Level

---
+++ Assets/Scripts/GameControlScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameControlScript : MonoBehaviour {
+
+    int GameCount = 0;
+
+    [SerializeField]
+    GameObject finalDoor;
+    // Use this for initialization
+    void Start()
+    {
+        if(GameCount == 0)
+        {
+            finalDoor.SetActive(false);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GameObject[] games = GameObject.FindGameObjectsWithTag("SubGame");
+        foreach(GameObject game in games)
+        {
+            Debug.Log("Found a game completion");
+            GameCount += 1;
+            Destroy(game);
+        }
+    }
+}
